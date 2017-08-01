95aac865ece2b345da353ea6dee3fb042b933caa
pkelechi <pkelechi@seamfix.com>
2017-08-01 16:55:01 +0100

    Add Student and staff classes to extend the user class, overidding some methods accordingly

--- user.js
+++ user.js
@@ -1,11 +1,12 @@
 'use strict';
 
-module.exports = class User {
-    constructor(firstname, lastname, emailAddress, phoneNumber){
+class User {
+    constructor(firstname, lastname, emailAddress, phoneNumber, userType){
         this.firstname = firstname;
         this.lastname = lastname;
         this.emailAddress = emailAddress;
         this.phoneNumber = phoneNumber;
+        this.userType = userType;
     }
 
     getFirstname(){
@@ -24,7 +25,61 @@ module.exports = class User {
         return this.phoneNumber;
     }
 
+    getUserType(){
+        return this.userType;
+    }
+
     showCredentials(){
         console.log('This is '+ this.firstname + ' ' + this.lastname+'. Email Address: '+' '+this.emailAddress+'. Phone Number: '+this.phoneNumber);
     }
+}
+
+
+class Staff extends User{
+    constructor(firstname, lastname, emailAddress, phoneNumber, teachingClass){
+        super(firstname, lastname, emailAddress, phoneNumber );
+        this.teachingClass = teachingClass;
+    }
+
+    getFullName(){
+        return this.lastname +' '+ this.firstname;
+    }
+
+    getTeachingClass(){
+        return this.class;
+    }  
+    getUserType(){
+        return 'STAFF';
+    }
+}
+
+class Student extends User{
+    constructor(firstname, lastname, emailAddress, phoneNumber, studentClass, age, category){
+        super(firstname, lastname, emailAddress, phoneNumber );
+        this.userType = 'STAFF';
+        this.studentClass = studentClass;
+        this.age = age;
+        this.category = category;
+    }
+
+    getFullName(){
+        return this.lastname +' '+ this.firstname;
+    }
+
+    getStudentClass(){
+        return this.studentClass;
+    }
+    getAge(){
+        return this.age;
+    }
+    getCategory(){
+        return this.category;
+    }
+    getUserType(){
+        return 'STUDENT';
+    }
+    changeAge(newAge){
+        this.age = newAge;
+    }
+
 }
