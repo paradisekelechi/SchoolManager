7f777bbd024b857ee7f117b931f6f31c0b13a50a
pkelechi <pkelechi@seamfix.com>
2017-08-01 17:42:16 +0100

    Add test cases for the classes

--- user.js
+++ user.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const chai = require('chai');
+const expect = chai.expect;
+
 class User {
     constructor(firstname, lastname, emailAddress, phoneNumber, userType){
         this.firstname = firstname;
@@ -70,7 +73,7 @@ class Student extends User{
         return this.studentClass;
     }
     getAge(){
-        return this.age;
+        return this.age + 'years';
     }
     getCategory(){
         return this.category;
@@ -81,7 +84,37 @@ class Student extends User{
     setAge(newAge){
         this.age = newAge;
     }
-    etStudentClass(newClass){
+    setStudentClass(newClass){
         this.studentClass = newClass;
     }
 }
+
+
+describe('Student Class:', () => {
+
+    //test the Student class that inherits from the User class
+    let oldStudent = new Student('Paradise', 'Kelechi', 'paradisekelechi@gmail.com', '07066748872', 'JSS2A', '13', 'Junior Class');
+    it('oldStudent.getAge() should return 13yrs', () =>{
+        expect(oldStudent.getAge()).to.equal('13years');
+    });
+    it('oldStudent.getFullName() should return "Kelechi Paradise"', () =>{
+        expect(oldStudent.getFullName()).to.equal('Kelechi Paradise');
+    });
+    //set student data
+    oldStudent.setStudentClass('SS2A');
+
+    it('oldStudent.getStudentClass() should return "SS2A"', () =>{
+        expect(oldStudent.getStudentClass()).to.equal('SS2A');
+    });
+
+
+    //test the Staff class that inherits from the User class
+    let newStaff = new Staff('Usman', 'Adamu', 'uadamu@yahoo.com', '09088999988', 'JS2A');    
+    it('newStaff.getUserType() should return "STAFF"', ()=>{
+        expect(newStaff.getUserType()).to.equal('STAFF');
+    });
+    it('newStaff.getFullName() should return "Adamu Usman"', ()=>{
+        expect(newStaff.getFullName()).to.equal('Adamu Usman');
+    });
+    
+});
